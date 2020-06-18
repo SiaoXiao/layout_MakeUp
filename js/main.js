e4a4d52b7b21616b8dc4f49307527b70f54a8ad2
SiaoXiao <kelu0427@gmail.com>
2020-06-18 23:53:36 +0800

    css update2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-  //------側邊欄開關------
+//增加none的CALSS
   function showHideHam() {
     let filterPanel = document.getElementsByClassName("list")[0];
     filterPanel.classList.toggle("none");
   }
-  //------側邊欄開關結束------
